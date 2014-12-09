e15f0734ddc63791def7a11d2df706a025a4293e
Adán Miguel Sánchez Albert <dual.3nigma@gmail.com>
2014-12-09 12:15:11 -0600

    integrar popover de ayuda

--- grails-app/assets/javascripts/layout.js
+++ grails-app/assets/javascripts/layout.js
@@ -5,4 +5,15 @@
 
 jQuery(function ($) {
     $('.signin-header').snowfall();
+
+    $('a.registration').popover({
+        animation: false,
+        trigger: 'manual',
+        html: true,
+        content: '¿Quieres organizar un intercambio? <strong>¡Regístrate!</strong>'
+    }).popover('show');
+
+    $(window).on('resize', function () {
+        $('a.registration').popover('show');
+    });
 });
