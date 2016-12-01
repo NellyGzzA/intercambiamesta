0323c8e9267bf4b7038190c1e069bee73c335d14
Nelly González <nelly.gonzalez@vincoorbis.com>
2016-11-30 19:13:46 -0600

    (md: 123017) Change mail service plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -62,12 +62,12 @@ grails.project.dependency.resolution = {
 		compile ':cache:1.1.8'
 		compile ":asset-pipeline:1.9.9"
 		compile ":spring-security-core:2.0-RC4"
-		compile ":mandrill:0.5"
+		compile ":mail:1.0.7"
 		
 		// plugins needed at runtime but not for compilation
 		runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
 		runtime ":jquery:1.11.1"
-        runtime ':twitter-bootstrap:3.3.1'
+		runtime ':twitter-bootstrap:3.3.1'
 
 		// Uncomment these to enable additional asset-pipeline capabilities
 		compile ":sass-asset-pipeline:1.9.0"
